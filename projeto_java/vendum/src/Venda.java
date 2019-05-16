818a1d6ecbac87cd6fd3af3fffb55cf66ca08a6a
rrpereira <contaricardo96@gmail.com>
2019-05-16 15:24:30 +0100

    ja esta?

---
+++ projeto_java/vendum/src/Venda.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+public class Venda implements IVenda, Serializable{
+    /*private String codV;
+    private double preco;
+
+    double getValor(){
+        return preco*quantidade;
+    }
+
+    +equals toString, +clone, +hashcode*/
+}
