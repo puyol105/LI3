586326c3184221cacf1fa2597d6a6aba380b504d
RitataPT <ritazilla@icloud.com>
2019-06-07 18:09:18 +0100

    InfoCliente e InfoVenda acabar de escrever, e ainda faltam algumas classes

--- projeto_java/vendum/src/InfoCliente.java
+++ projeto_java/vendum/src/InfoCliente.java
@@ -3,32 +3,60 @@ public class InfoCliente {
     private int quantidade;
     private double preco;
 
+    /**
+     * Construtor vazio.
+     */
     public InfoCliente(){
         this.quantidade = 0;
         this.preco = 0.0;
     }
 
+    /**
+     * Construtor parametrizado.
+     * @param quantidade
+     * @param preco
+     */
     public InfoCliente(int quantidade, double preco){
         this.quantidade = quantidade;
         this.preco = preco;
     }
 
+    /**
+     * Método que devolve a quantidade
+     * @return
+     */
     public int getQuantidade() {
         return quantidade;
     }
 
+    /**
+     * Método que define a quantidade
+     * @param quantidade
+     */
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
 
+    /**
+     * Método que devolve o
+     * @return
+     */
     public double getPreco() {
         return preco;
     }
 
+    /**
+     * Método que define 
+     * @param preco
+     */
     public void setPreco(double preco) {
         this.preco = preco;
     }
 
+    /**
+     * Método que calcula 
+     * @return
+     */
     public double total_infocliente(){
         return ((double) this.quantidade*this.preco);
     }
--- projeto_java/vendum/src/InfoVenda.java
+++ projeto_java/vendum/src/InfoVenda.java
@@ -5,32 +5,60 @@ public class InfoVenda implements java.io.Serializable{
     private int quantidade;
     private double preco;
 
+    /**
+     * Construtor vazio.
+     */
     public InfoVenda(){
         this.quantidade = 0;
         this.preco = 0.0;
     }
 
+    /**
+     * Construtor parametrizado.
+     * @param quantidade
+     * @param preco
+     */
     public InfoVenda(int quantidade, double preco){
         this.quantidade = quantidade;
         this.preco = preco;
     }
 
+    /**
+     * Método que devolve a quantidade
+     * @return
+     */
     public int getQuantidade() {
         return quantidade;
     }
 
+    /**
+     * Método que define a quantidade
+     * @param quantidade
+     */
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
 
+    /**
+     * Método que devolve o preco
+     * @return
+     */
     public double getPreco() {
         return preco;
     }
 
+    /**
+     * Método que define o preco
+     * @param preco
+     */
     public void setPreco(double preco) {
         this.preco = preco;
     }
 
+    /**
+     * Método que calcula
+     * @return
+     */
     public double total_infovenda(){
         return ((double) this.quantidade*this.preco);
     }
